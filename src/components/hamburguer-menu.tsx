453fae1ebd1574a0798ea2924119ed26de98b064
Matheus Amon <amonmatheus757@gmail.com>
2024-05-14 09:01:36 -0300

    feat: add trigger to sheet

--- src/components/hamburguer-menu.tsx
+++ src/components/hamburguer-menu.tsx
@@ -1,48 +1,53 @@
-import { Sheet, SheetContent } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import HamburguerMenuItem from "./hamburguer-menu-item";
+import { MenuIcon } from "lucide-react";
 
 const HamburguerMenu = () => {
-
     return ( 
-        <Sheet open>
-            <SheetContent className="flex flex-col items-start">    
-                <HamburguerMenuItem
-                    title="Projetos"
-                    items={
-                        [
-                            "Apresentação Pessoal",
-                            "Layout Responsivo",
-                            "Formulário de Contato",
-                            "Galeria de Imagens",
-                        ]
-                    }
-                />
+        <>
+            <Sheet>
+                <SheetTrigger>
+                    <MenuIcon className="h-6 w-6" />
+                </SheetTrigger>
+                <SheetContent className="flex flex-col items-start">    
+                    <HamburguerMenuItem
+                        title="Projetos"
+                        items={
+                            [
+                                "Apresentação Pessoal",
+                                "Layout Responsivo",
+                                "Formulário de Contato",
+                                "Galeria de Imagens",
+                            ]
+                        }
+                    />
 
-                <HamburguerMenuItem
-                    title="Social"
-                    items={
-                        [
-                            "LinkedIn",
-                            "GitHub",
-                            "Behance",
-                        ]
-                    }
-                />
+                    <HamburguerMenuItem
+                        title="Social"
+                        items={
+                            [
+                                "LinkedIn",
+                                "GitHub",
+                                "Behance",
+                            ]
+                        }
+                    />
 
-                <HamburguerMenuItem
-                    title="Certificações"
-                    items={
-                        [
-                            "Formação FullStack JS",
-                            "Imersão Frontend",
-                            "Imersão MultiCloud",
-                            "Figma",
-                            "Photoshop",
-                        ]
-                    }
-                />
-            </SheetContent>
-        </Sheet>
+                    <HamburguerMenuItem
+                        title="Certificações"
+                        items={
+                            [
+                                "Formação FullStack JS",
+                                "Imersão Frontend",
+                                "Imersão MultiCloud",
+                                "Figma",
+                                "Photoshop",
+                            ]
+                        }
+                    />
+                </SheetContent>
+            </Sheet>
+        </>
      );
 }
  
